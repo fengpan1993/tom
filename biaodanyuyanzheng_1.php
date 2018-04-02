014f74c40676e8a0ebe5c75aea29b269cb1077b2
fengpan1993 <15988410887@163.com>
2018-04-02 15:59:20 +0800

    zhushi

---
+++ biaodanyuyanzheng_1.php
@@ -0,0 +1,22 @@
+<form method="post" action="Demo4.php">
+    姓名：<input type="text" name="username"/><br/>
+    <input type="submit" value="提交"/>
+</form>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengpan
+ * Date: 2018/3/25
+ * Time: 21:24
+ */
+  //重新导向URL
+//  header('Location:test1.php');
+//echo 'baidu.com';
+//header('Location:http://www.baidu.com');
+
+//字符编码
+
+//设置页面编码
+//header('Content-Type:text/html;charset=gbk');
+//echo'中文';
+?>
